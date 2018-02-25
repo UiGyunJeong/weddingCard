f306059594cad6f6964f5e9d4afbed615fe26ace
UiGyun.JEong <ui.gyun.jeong@gmail.com>
2018-02-26 01:28:48 +0900

    add js

---
+++ resources/js/script.js
@@ -0,0 +1,74 @@
+    $('.js-2014').click(function() {
+        var nav = $('.y2014');
+        var icon = $('.i-2014');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-chevron-down')) {
+            icon.addClass('ion-chevron-up');
+            icon.removeClass('ion-chevron-down');
+        } else {
+            icon.addClass('ion-chevron-down');
+            icon.removeClass('ion-chevron-up');
+        }        
+    });
+
+    $('.js-2015').click(function() {
+        var nav = $('.y2015');
+        var icon = $('.i-2015');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-chevron-down')) {
+            icon.addClass('ion-chevron-up');
+            icon.removeClass('ion-chevron-down');
+        } else {
+            icon.addClass('ion-chevron-down');
+            icon.removeClass('ion-chevron-up');
+        }        
+    });
+
+    $('.js-2016').click(function() {
+        var nav = $('.y2016');
+        var icon = $('.i-2016');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-chevron-down')) {
+            icon.addClass('ion-chevron-up');
+            icon.removeClass('ion-chevron-down');
+        } else {
+            icon.addClass('ion-chevron-down');
+            icon.removeClass('ion-chevron-up');
+        }        
+    });
+
+    $('.js-2017').click(function() {
+        var nav = $('.y2017');
+        var icon = $('.i-2017');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-chevron-down')) {
+            icon.addClass('ion-chevron-up');
+            icon.removeClass('ion-chevron-down');
+        } else {
+            icon.addClass('ion-chevron-down');
+            icon.removeClass('ion-chevron-up');
+        }        
+    });
+
+    $('.js-2018').click(function() {
+        var nav = $('.y2018');
+        var icon = $('.i-2018');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-chevron-up')) {
+            icon.addClass('ion-chevron-down');
+            icon.removeClass('ion-chevron-up');
+        } else {
+            icon.addClass('ion-chevron-up');
+            icon.removeClass('ion-chevron-down');
+        }        
+    });
