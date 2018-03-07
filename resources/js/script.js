b9b74bd0ce4c3934975217186cb2ac13d3ab9fdf
UiGyun Jeong <ui.gyun.jeong@gmail.com>
2018-03-07 12:41:05 +0900

    add caption for img + img bug fix

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,8 @@
     $('.js-2014').click(function() {
-        var photo = $('.y2014');
+        var photo = $('.js2014');
         var icon = $('.i-2014');
         
-        photo.slideToggle(400);
+        photo.slideToggle(300);
         
         if (icon.hasClass('ion-chevron-down')) {
             icon.addClass('ion-chevron-up');
@@ -14,10 +14,10 @@
     });
 
     $('.js-2015').click(function() {
-        var photo = $('.y2015');
+        var photo = $('.js2015');
         var icon = $('.i-2015');
         
-        photo.slideToggle(400);
+        photo.slideToggle(300);
         
         if (icon.hasClass('ion-chevron-down')) {
             icon.addClass('ion-chevron-up');
@@ -29,10 +29,10 @@
     });
 
     $('.js-2016').click(function() {
-        var photo = $('.y2016');
+        var photo = $('.js2016');
         var icon = $('.i-2016');
         
-        photo.slideToggle(400);
+        photo.slideToggle(300);
         
         if (icon.hasClass('ion-chevron-down')) {
             icon.addClass('ion-chevron-up');
@@ -44,10 +44,10 @@
     });
 
     $('.js-2017').click(function() {
-        var photo = $('.y2017');
+        var photo = $('.js2017');
         var icon = $('.i-2017');
         
-        photo.slideToggle(400);
+        photo.slideToggle(300);
         
         if (icon.hasClass('ion-chevron-down')) {
             icon.addClass('ion-chevron-up');
@@ -59,10 +59,10 @@
     });
 
     $('.js-2018').click(function() {
-        var photo = $('.y2018');
+        var photo = $('.js2018');
         var icon = $('.i-2018');
         
-        photo.slideToggle(200);
+        photo.slideToggle(300);
         
         if (icon.hasClass('ion-chevron-up')) {
             icon.addClass('ion-chevron-down');
